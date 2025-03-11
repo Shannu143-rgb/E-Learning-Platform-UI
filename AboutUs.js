99718fa56034cd891057df8b344d26a2e131a0a4
Shannu143-rgb <shannushaik305@gmail.com>
2025-03-11 10:16:42 +0530

    Create AboutUs.js

---
+++ AboutUs.js
@@ -0,0 +1,80 @@
+function About(){
+    return(
+    <>
+    <div className="about-container">
+        <p className="team-name">About Us</p>
+        <p>
+          At our platform, we believe that learning should be <b>accessible</b>,
+          engaging, and empowering for everyone. Our platform is designed to
+          provide <b>high-quality</b>, interactive, and industry-relevant
+          courses to help learners of all backgrounds achieve their goals.
+        </p>
+        <p>
+          Our mission is to bridge the gap between <b>education and skills</b>{" "}
+          by offering practical courses aligned with industry needs. Whether
+          you're a <b>student</b>, professional, or lifelong learner, we provide
+          the resources you need to <b>upskill</b> and succeed.
+        </p>
+    </div>
+
+    <p className="team-name">Our Team</p>
+
+    <p className="team-content">
+    Meet the passionate educators and industry experts behind our e-learning platform. 
+    Our team is dedicated to delivering high-quality courses that help learners upskill and succeed in their careers.
+    </p>
+    
+    <div className="Team">
+        <div className="team-mem">
+            <img src="pro1.avif"/>
+            <p>Miss.Jessie</p>
+        </div>
+        
+        <div className="team-mem">
+            <img src="pro2.jpeg"/>
+            <p>Miss.Sludha</p>
+        </div>
+        
+        <div className="team-mem">
+            <img src="pro3.jpg"/>
+            <p>Miss.Taniya</p>
+        </div>
+        
+        <div className="team-mem">
+            <img src="pro7.avif"/>
+            <p>Mr.Sanjay</p>
+        </div>
+
+        <div className="team-mem">
+            <img src="pro5.jpg"/>
+            <p>Mr.Thilak</p>
+        </div>
+        
+        <div className="team-mem">
+            <img src="pro6.jpg"/>
+            <p>Mr.Jenny</p>
+        </div>
+        
+
+    </div>
+
+    <div className="choose-us">
+        <h4>Why Choose Us?</h4>
+            
+            <div className="points">
+                <p className="para">✅ <b>Expert Instructors</b> – Learn from industry professionals.</p>
+                <p className="para">✅ <b>Flexible Learning</b> – Study anytime, anywhere.</p>
+                <p className="para">✅ <b>Hands-on Projects</b> – Gain real-world experience.</p>
+                <p className="para">✅ <b>Certifications</b> – Earn certificates to boost your career.</p>
+            
+            </div>
+        
+        </div>
+    
+          
+ </>
+    )
+
+}
+
+export default About
